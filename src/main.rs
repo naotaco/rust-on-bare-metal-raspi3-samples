12f2822921321f9a5bb4bcbeedbd2b38e5ccf8f5
Nao Taco <naotaco@gmail.com>
2019-09-07 16:58:14 +0900

    add timer

--- src/main.rs
+++ src/main.rs
@@ -34,6 +34,7 @@ mod dmac;
 mod gpio;
 mod interrupt;
 mod mbox;
+mod timer;
 mod uart;
 
 fn init(data_addr: u32, size: usize, init_data: u32) {
@@ -94,6 +95,13 @@ fn memcpy_cpu(src: u32, dest: u32, size: usize) {
     }
 }
 
+fn print_time(uart: &uart::Uart) {
+    let timer = timer::TIMER::new();
+    uart.puts("time: ");
+    uart.hex(timer.get_counter32());
+    uart.puts("\n");
+}
+
 fn kernel_entry() -> ! {
     arm_debug::setup_debug();
 
@@ -114,16 +122,21 @@ fn kernel_entry() -> ! {
 
     let src = 0x200_0000;
     let dest = 0x400_0000;
-    let size = 0x100_0000;
+    let size = 0x1_0000;
+    print_time(&uart);
     uart.puts("Initializing...\n");
     init(src, size, 0xFF00_0000);
     init(dest, size, 0x1200_0000);
+
+    print_time(&uart);
     uart.puts("Initializing.......done! \n");
+
     dump(src, size, &uart);
     dump(dest, size, &uart);
 
     // memcpy_dmac(src, dest, size);
     memcpy_cpu(src, dest, size);
+    print_time(&uart);
     uart.puts("Done!\n");
     dump(dest, size, &uart);
 
